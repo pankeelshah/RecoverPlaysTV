28b0dc19d3fcc01083ebe2f5fe05529e99064d71
Pankeel Shah <shahx317@umn.edu>
2020-06-05 23:07:13 -0500

    added comments

--- test.py
+++ test.py
@@ -2,17 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 import urllib.request
 
-user_profile_page_url = "https://web.archive.org/web/20191210043532/https://plays.tv/u/"
+# input username
 username = "bluepancake69"
+
+# plays.tv profile link
+user_profile_page_url = "https://web.archive.org/web/20191210043532/https://plays.tv/u/"
 user_profile_page_url += username
 page = requests.get(user_profile_page_url)
 soup = BeautifulSoup(page.text, 'html.parser')
 clip_link_list = soup.findAll(class_='thumb-link')
-s = "https://web.archive.org"
 links = []
 
 for elem in clip_link_list:
-    link = s + elem.get("href")
+    # link of clip
+    link = "https://web.archive.org" + elem.get("href")
+
+    # get video link for download of clip
     index = link.find("?")
     link = link[:index]
     page = requests.get(link)
@@ -22,10 +27,12 @@ for elem in clip_link_list:
     while n[index] != '\"':
         index -= 1
     video_link = "https:" + n[index + 1:index_end+7]
-    # print(video_link)
     links.append(video_link)
 
+# remove duplicate links
 links = list(set(links))
+
+# different video names
 video_increment = 1
 for link in links:
     try:
