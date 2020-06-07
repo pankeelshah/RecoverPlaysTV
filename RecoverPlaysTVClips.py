05245e7bed7f37376e25b052d84c78d38297bdc3
Pankeel Shah <shahx317@umn.edu>
2020-06-07 17:43:39 -0500

    added clip names to mp4 files

--- RecoverPlaysTVClips.py
+++ RecoverPlaysTVClips.py
@@ -15,14 +15,17 @@ def create_zip(username):
     soup = bs4.BeautifulSoup(page.text, 'html.parser')
     clip_link_list = soup.findAll(class_='thumb-link')
     links = []
-    print("Going through clips\n")
+
     for elem in clip_link_list:
         # link of clip
         link = "https://web.archive.org" + elem.get("href")
-
         # get video link for download of clip
         index = link.find("?")
         link = link[:index]
+        # get name of clip
+        index = link.rfind("/")
+        clip_name = link[index + 1:]
+        # go to clip page to get download link
         page = requests.get(link)
         n = str(page.content, 'utf-8')
         index = n.find("720.mp4")
@@ -30,28 +33,26 @@ def create_zip(username):
         while n[index] != '\"':
             index -= 1
         video_link = "https:" + n[index + 1:index_end+7]
-        links.append(video_link)
+        links.append((video_link, clip_name))
 
     # remove duplicate links
     links = list(set(links))
 
     # different video names
-    video_increment = 1
-    print("Going through links\n")
+    video_increment = 0
     for link in links:
         try:
-            urllib.request.urlretrieve(link, "clip" + str(video_increment) + ".mp4")
+            urllib.request.urlretrieve(link[0], link[1] + ".mp4")
             video_increment += 1
         except:
             print("not a video link")
 
     # create zip file
-    print("creating zip\n")
-    zipObj = zipfile.ZipFile('static/PlaysTVClips.zip', 'w')
+    zipObj = zipfile.ZipFile('static/' + username + '_PlaysTVClips.zip', 'w')
     
     # Add multiple files to the zip
-    for i in range(1, video_increment):
-        zipObj.write("clip" + str(i) + ".mp4")
+    for i in range(0, video_increment):
+        zipObj.write(links[i][1] + ".mp4")
 
     # close the Zip File
     zipObj.close()
@@ -59,4 +60,3 @@ def create_zip(username):
     # remove video files
     for video in glob.glob("*.mp4"):
         os.remove(video)
-
