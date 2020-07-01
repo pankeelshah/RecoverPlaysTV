19c7b1db0130b6abca3f37299c199094cea48ea9
Pankeel Shah <shahx317@umn.edu>
2020-07-01 08:30:22 -0500

    changed 1 http to https

--- static/download.js
+++ static/download.js
@@ -1,5 +1,5 @@
 var id;
-var socket = io.connect("http://" + document.domain + ":" + location.port);
+var socket = io.connect("https://" + document.domain + ":" + location.port);
 var status = 0;
 var username;
 
